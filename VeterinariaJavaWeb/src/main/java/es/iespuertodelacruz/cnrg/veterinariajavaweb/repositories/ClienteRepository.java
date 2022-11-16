0534eeddbdc5b63d0ae606535eb1ee70fa6d98cf
Christian Novo Sacramento <christianivh366@gmail.com>
2022-11-16 00:05:09 +0000

    feat: Crea findById - ClienteRepository
    Resolves: #6

---
+++ VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/ClienteRepository.java
@@ -0,0 +1,78 @@
+package es.iespuertodelacruz.cnrg.veterinariajavaweb.repositories;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import es.iespuertodelacruz.cnrg.veterinariajavaweb.entities.Cliente;
+
+public class ClienteRepository implements ICrud<Cliente, String>{
+
+	
+	private final EntityManagerFactory entityManagerFactory;
+	
+	/**
+	 * Constructor que recibe el entityManagerFactory
+	 * @param entityManagerFactory encargado de abrir la conexion DDBB
+	 */
+	public ClienteRepository(EntityManagerFactory entityManagerFactory) {
+		this.entityManagerFactory = entityManagerFactory;
+	}
+	
+	/**
+	 * Metodo que guarda el cliente en la DDBB
+	 * @param dao Cliente que se quiere guardar
+	 */
+	@Override
+	public Cliente save(Cliente dao) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Metodo que busca el cliente en la DDBB
+	 * @param id indentificador del cliente
+	 */
+	@Override
+	public Cliente findById(String id) {
+		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Cliente cliente = entityManager.find(Cliente.class, id);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		
+		return cliente;
+	}
+
+	/**
+	 * Metodo que actualiza los datos del cliente en la DDBB
+	 * @param dao Cliente que se quiere actualizar
+	 */
+	@Override
+	public boolean update(Cliente dao) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * Metodo que elimina el cliente en la DDBB
+	 * @param id identificador del cliente que se quiere borrar
+	 */
+	@Override
+	public boolean delete(String id) {
+		
+		return false;
+	}
+
+	/**
+	 * Metodo que devuelve todos los clientes de la DDBB
+	 */
+	@Override
+	public ArrayList<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
