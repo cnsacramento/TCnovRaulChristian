665153c1cd62276e1835638fb05c9c0d56a2d614
Christian Novo Sacramento <christianivh366@gmail.com>
2022-11-17 23:06:02 +0000

    fix: Cambia estrategia de GeneratedValue
    Confusión con AUTO e IDENTITY por lo que se realiza una permetución
    
    Resolves: #25

--- VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/entities/Mascota.java
+++ VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/entities/Mascota.java
@@ -18,7 +18,7 @@ public class Mascota implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 
 	@Column(name="fecha_nacimiento")
