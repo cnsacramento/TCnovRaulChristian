3a014fa0efef19d4e4d99af94cc9fb80ea3d7e98
Christian Novo Sacramento <christianivh366@gmail.com>
2022-11-18 11:30:24 +0000

    feat: findById de TipoIntervencion
    Resolves: #9

---
+++ VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/TipoIntervencionRepository.java
@@ -0,0 +1,47 @@
+package es.iespuertodelacruz.cnrg.veterinariajavaweb.repositories;
+
+import es.iespuertodelacruz.cnrg.veterinariajavaweb.entities.TipoIntervencion;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class TipoIntervencionRepository implements ICrud<TipoIntervencion, Integer> {
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    public TipoIntervencionRepository(final EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Override
+    public TipoIntervencion save(TipoIntervencion dao) {
+        return null;
+    }
+
+    @Override
+    public TipoIntervencion findById(Integer id) {
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        TipoIntervencion tipoIntervencion = entityManager.find(TipoIntervencion.class, id);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        return tipoIntervencion;
+    }
+
+    @Override
+    public boolean update(TipoIntervencion dao) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return false;
+    }
+
+    @Override
+    public List<TipoIntervencion> findAll() {
+        return null;
+    }
+}
