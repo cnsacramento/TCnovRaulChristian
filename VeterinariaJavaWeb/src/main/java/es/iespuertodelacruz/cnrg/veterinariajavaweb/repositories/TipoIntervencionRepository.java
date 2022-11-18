749f06661660d643273cc223321bf9e46302b956
Christian Novo Sacramento <christianivh366@gmail.com>
2022-11-18 11:40:20 +0000

    feat: Save de TipoIntervencionRepository
    Resolves: #9

--- VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/TipoIntervencionRepository.java
+++ VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/TipoIntervencionRepository.java
@@ -16,6 +16,18 @@ public class TipoIntervencionRepository implements ICrud<TipoIntervencion, Integ
 
     @Override
     public TipoIntervencion save(TipoIntervencion dao) {
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(dao);
+            entityManager.getTransaction().commit();
+            return dao;
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            entityManager.close();
+        }
         return null;
     }
 
