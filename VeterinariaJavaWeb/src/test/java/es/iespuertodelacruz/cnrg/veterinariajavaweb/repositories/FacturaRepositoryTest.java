69abeacbaef68e78809f846439b53b3ee3f9392c
raul <raulglezmartin@gmail.com>
2022-11-19 11:50:04 +0000

    feat: se crea la clase FacturaRepositoryTest Resolves: #33

---
+++ VeterinariaJavaWeb/src/test/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/FacturaRepositoryTest.java
@@ -0,0 +1,44 @@
+package es.iespuertodelacruz.cnrg.veterinariajavaweb.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class FacturaRepositoryTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+}
