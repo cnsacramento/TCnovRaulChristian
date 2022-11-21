dba4860cadecbc58b6b255b776cf0630f895ea35
raul <raulglezmartin@gmail.com>
2022-11-21 23:42:01 +0000

    feat: se crea findById

--- VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/VeterinarioRepository.java
+++ VeterinariaJavaWeb/src/main/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/VeterinarioRepository.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.RollbackException;
 
+import es.iespuertodelacruz.cnrg.veterinariajavaweb.entities.TipoRestriccionDia;
 import es.iespuertodelacruz.cnrg.veterinariajavaweb.entities.Veterinario;
 
 public class VeterinarioRepository implements ICrud<Veterinario, String> {
@@ -35,19 +36,23 @@ public class VeterinarioRepository implements ICrud<Veterinario, String> {
 	}
 
 	@Override
-	public Veterinario findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Veterinario findById(String dni) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Veterinario veterinario = entityManager.find(Veterinario.class, dni);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		return veterinario;
 	}
 
 	@Override
-	public boolean update(Veterinario dao) {
+	public boolean update(Veterinario veterinario) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean delete(String id) {
+	public boolean delete(String dni) {
 		// TODO Auto-generated method stub
 		return false;
 	}
