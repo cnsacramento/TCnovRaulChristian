813400c3f7204dc5481b4def241e8d6e6f39e663
raul <raulglezmartin@gmail.com>
2022-11-22 00:20:08 +0000

    feat: se finaliza la clase VeterinarioRepositoryTest

--- VeterinariaJavaWeb/src/test/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/VeterinarioRepositoryTest.java
+++ VeterinariaJavaWeb/src/test/java/es/iespuertodelacruz/cnrg/veterinariajavaweb/repositories/VeterinarioRepositoryTest.java
@@ -19,7 +19,9 @@ import es.iespuertodelacruz.cnrg.veterinariajavaweb.entities.Veterinario;
 class VeterinarioRepositoryTest {
 	
 	static VeterinarioRepository veterinarioRepository;
-
+	static CuentaVeterinarioRepository cuentaVeterinarioRepository;
+	static EspecialidadVeterinarioRepository especialidadVeterinarioRepository;
+	
 	static Veterinario veterinario1;
 	static Veterinario veterinario2;
 	static CuentaVeterinario cuenta1;
@@ -31,21 +33,27 @@ class VeterinarioRepositoryTest {
 	static void setUpBeforeClass() throws Exception {
 		EntityManagerFactory entityManagerFactory = EntityManagerFactorySingleton.getInstance().getEmf();
 		veterinarioRepository = new VeterinarioRepository(entityManagerFactory);
-
+		cuentaVeterinarioRepository = new CuentaVeterinarioRepository(entityManagerFactory);
+		especialidadVeterinarioRepository = new EspecialidadVeterinarioRepository(entityManagerFactory);
+		
+		
 		cuenta1 = new CuentaVeterinario();
 		cuenta1.setCorreo("correo1@gmail.com");
 		cuenta1.setContrasenia("1q2w3e4r");
+		cuentaVeterinarioRepository.save(cuenta1);
 		
 		cuenta2 = new CuentaVeterinario();
 		cuenta2.setCorreo("correo2@gmail.com");
 		cuenta2.setContrasenia("1q2w3e4r");
+		cuentaVeterinarioRepository.save(cuenta2);
 		
 		especialidad1 = new EspecialidadVeterinario();
 		especialidad1.setNombre("ESPECIALIDAD1");
+		especialidadVeterinarioRepository.save(especialidad1);
 		
 		especialidad2 = new EspecialidadVeterinario();
 		especialidad2.setNombre("ESPECIALIDAD2");
-		
+		especialidadVeterinarioRepository.save(especialidad2);
 		// TIPO 1
 		veterinario1 = new Veterinario();
 		veterinario1.setDni("12345678z");
@@ -106,14 +114,14 @@ class VeterinarioRepositoryTest {
 	@Test
 	@Order(5)
 	void testDelete() {
-		assertTrue(tipoRestriccionDiaRepository.delete(tipo1.getTipo()), "Si ningun dia lo tiene asigado se debería borrar");
-		assertFalse(tipoRestriccionDiaRepository.delete(tipo1.getTipo()), "Si algun dia lo tiene asigado no se debería borrar");
+		assertTrue(veterinarioRepository.delete(veterinario1.getDni()), "Si ningun equipoDeIntervencion lo tiene asigado se debería borrar");
+		assertFalse(veterinarioRepository.delete(veterinario1.getDni()), "Si algun equipoDeIntervencion lo tiene asigado no se debería borrar y si no existe no se debería borrar");
 	}
 
 	@Test
 	@Order(4)
 	void testFindAll() {
-		assertTrue(tipoRestriccionDiaRepository.findAll().size() > 0, "El tamanio del array debe ser mayor que cero");
+		assertTrue(veterinarioRepository.findAll().size() > 0, "El tamanio del array debe ser mayor que cero");
 	}
 
 }
